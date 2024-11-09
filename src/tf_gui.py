cf4c22d9233ffa974a9eaa2cd4410a5b4bac17d5
Arian-G <arian.goudar@gmail.com>
2024-11-09 00:21:51 -0600

    Add theme and axis selection(). Change transformations

--- src/tf_gui.py
+++ src/tf_gui.py
@@ -15,6 +15,7 @@ class tf_gui:
         self.create_transformations_menu( )
         self.create_canvas( )
         self.create_status( )
+        self.create_theme( )
         self.main_loop( )
 
     def main_loop( self ):
@@ -47,9 +48,9 @@ class tf_gui:
             dpg.add_text( "" )
 
         with dpg.group( horizontal = False, parent = settings_menu ):
-            with dpg.tooltip( dpg.add_button( label = "Reset Camera" )):
+            with dpg.tooltip( dpg.add_button( label = "Reset Camera", width = 100 )):
                 dpg.add_text( "Resets the camera to its initial definition." )
-            with dpg.tooltip(  dpg.add_button( label = "Reset Object" )):
+            with dpg.tooltip( dpg.add_button( label = "Reset Object", width = 100 )):
                 dpg.add_text( "Resets the object to its initial definition." )
 
         ##### About Menu
@@ -80,58 +81,93 @@ class tf_gui:
         transformation_menu = dpg.add_tree_node( label = "Transformations", parent = self.root )
         ##### Translation
         with dpg.tree_node( label = "Translation", parent = transformation_menu ):
-            with dpg.group( horizontal = True ):
-                dpg.add_text( "X Axis (Dx):" )
-                dpg.add_input_float( default_value = 0.0, width = 100, step = 1, 
-                                     callback = lambda s: self.enforce_bounds( s, -10, 10 ), format = "%.1f" )
-                dpg.add_text( "Y Axis (Dy):" )
-                dpg.add_input_float( default_value = 0.0, width = 100, step = 1, 
-                                     callback = lambda s: self.enforce_bounds( s, -10, 10 ), format = "%.1f" )
-                dpg.add_text( "Z Axis (Dz):" )
-                dpg.add_input_float( default_value = 0.0, width = 100, step = 1, 
-                                     callback = lambda s: self.enforce_bounds( s, -10, 10 ), format = "%.1f" )
-            dpg.add_button( label = "Translate" )
-            # with dpg.group( horizontal = True, parent = transformation_menu ): dpg.add_text( "" ) # Spacing
+            with dpg.group( horizontal = False ):
+                with dpg.group( horizontal = True ):
+                    dpg.add_text( "Dx:" )
+                    dpg.add_input_float( default_value = 0.0, width = 100, step = 1, 
+                                        callback = lambda s: self.enforce_bounds( s, -10, 10 ), format = "%.1f" )
+                dpg.add_spacer( height = 5 )
+                with dpg.group( horizontal = True ):
+                    dpg.add_text( "Dy:" )
+                    dpg.add_input_float( default_value = 0.0, width = 100, step = 1, 
+                                        callback = lambda s: self.enforce_bounds( s, -10, 10 ), format = "%.1f" )
+                dpg.add_spacer( height = 5 )
+                with dpg.group( horizontal = True ):
+                    dpg.add_text( "Dz:" )
+                    dpg.add_input_float( default_value = 0.0, width = 100, step = 1, 
+                                        callback = lambda s: self.enforce_bounds( s, -10, 10 ), format = "%.1f" )
+                dpg.add_spacer( height = 5 )
+            dpg.add_button( label = "Translate", width = 80 )
+            dpg.add_spacer( height = 10 )
 
         ##### Scaling
         with dpg.tree_node( label = "Scaling", parent = transformation_menu ):
             with dpg.group( horizontal = True ):
-                dpg.add_text( "X Axis (Sx):" )
+                with dpg.tooltip( dpg.add_text( "About (x, y, z):" )):
+                    dpg.add_text( "The point at which to scale about.\nDefault at origin." )
+                dpg.add_input_text( hint = "0, 0, 0", width = 100 )
+            dpg.add_spacer( height = 5 )
+            with dpg.group( horizontal = True ):
+                dpg.add_text( "Sx:" )
                 dpg.add_input_float( default_value = 1.0, width = 100, step = 0.1,
                                      callback = lambda s: self.enforce_bounds( s, 0.1, 10 ), format = "%.1f" )
-                dpg.add_text( "Y Axis (Sy):" )
+            dpg.add_spacer( height = 5 )
+            with dpg.group( horizontal = True ):
+                dpg.add_text( "Sy:" )
                 dpg.add_input_float( default_value = 1.0, width = 100, step = 0.1, 
                                      callback = lambda s: self.enforce_bounds( s, 0.1, 10 ), format = "%.1f" )
-                dpg.add_text( "Z Axis (Sz):" )
+            dpg.add_spacer( height = 5 )
+            with dpg.group( horizontal = True ):
+                dpg.add_text( "Sz:" )
                 dpg.add_input_float( default_value = 1.0, width = 100, step = 0.1,
                                      callback = lambda s: self.enforce_bounds( s, 0.1, 10 ), format = "%.1f" )
-            dpg.add_button( label = "Scale    " )
-            # with dpg.group( horizontal = True, parent = transformation_menu ): dpg.add_text( "" ) # Spacing
+            dpg.add_spacer( height = 5 )
+            dpg.add_button( label = "Scale", width = 80 )
+            dpg.add_spacer( height = 10 )
 
         ##### Rotation
         with dpg.tree_node( label = "Rotation", parent = transformation_menu ):
             with dpg.group( horizontal = True ):
-                dpg.add_text( "X Axis (Rx):" )
+                dpg.add_text( "About:" )
+                dpg.add_radio_button( items = [ "X-axis", "Y-axis", "Z-axis", "Line AB" ], horizontal = True,
+                                      callback = self.axis_selection, tag = "axis_selector" )
+            dpg.add_spacer( height = 5 )
+            with dpg.group( horizontal = True ):
+                dpg.add_text( "A (x, y, z):" )
+                dpg.add_input_text( hint = "0, 0, 0", width = 100 )
+            dpg.add_spacer( height = 5 )
+            with dpg.group( horizontal = True ):
+                dpg.add_text( "B (x, y, z):" )
+                dpg.add_input_text( hint = "1, 1, 1", width = 100 )
+            dpg.add_spacer( height = 5 )
+            with dpg.group( horizontal = True ):
+                dpg.add_text( "Rx:", tag = "deg_label" )
                 dpg.add_input_float( default_value = 0.0, width = 100, step = 1,
                                      callback = lambda s: self.enforce_bounds( s, -360, 360 ), format = "%.1f" )
-                dpg.add_text( "Y Axis (Ry):" )
-                dpg.add_input_float( default_value = 0.0, width = 100, step = 1,
-                                     callback = lambda s: self.enforce_bounds( s, -360, 360 ), format = "%.1f" )
-                dpg.add_text( "Z Axis (Rz):" )
-                dpg.add_input_float( default_value = 0.0, width = 100, step = 1,
-                                     callback = lambda s: self.enforce_bounds( s, -360, 360 ), format = "%.1f" )
-            dpg.add_button( label = "Rotate   " )
-            # with dpg.group( horizontal = True, parent = transformation_menu ): dpg.add_text( "" ) # Spacing
+            dpg.add_spacer( height = 5 )
+            dpg.add_button( label = "Rotate", width = 80 )
+            dpg.add_spacer( height = 10 )
+
+    def axis_selection( self, sender ):
+        curr_axis = dpg.get_value( sender )
+        if curr_axis == "X-axis":
+            dpg.set_value( "deg_label", "Rx:" )
+        elif curr_axis == "Y-axis":
+            dpg.set_value( "deg_label", "Ry:" )
+        elif curr_axis == "Z-axis":
+            dpg.set_value( "deg_label", "Rz:" )
+        else:
+            dpg.set_value( "deg_label", "Rv:" )
 
     def create_canvas( self ):
         canvas_menu = dpg.add_tree_node( label = "Canvas", parent = self.root, selectable = True )
         with dpg.group( parent = canvas_menu ):
             with dpg.drawlist( width = 1920, height = 500, tag = "Canvas" ):
-                dpg.draw_rectangle(( 100, 100 ), ( 200, 200 ), color = ( 255, 0, 0, 255 ), fill =( 200, 0, 0, 150 ))
+                dpg.draw_rectangle(( 100, 100 ), ( 200, 200 ), color = ( 255, 0, 0, 255 ), fill = ( 200, 0, 0, 150 ))
 
     def create_status( self ):
         with dpg.window( label = "Error", modal = True, show = False, tag = "status", width = 100, height = 100 ):
-            self.status_text = dpg.add_text("")
+            self.status_text = dpg.add_text( "" )
 
     def show_status( self, s ):
         dpg.set_value( self.status_text, s )
@@ -158,5 +194,63 @@ class tf_gui:
     
     def save_as( self, _, app_data ):
         pass
+
+    def create_theme( self ):    
+        prim_color = ( 66, 165, 245 )
+        light_bg = ( 33, 33, 33 )
+        darker_bg = ( 55, 55, 55 )
+        text_color = (220, 220, 220)
+
+        with dpg.theme( ) as global_theme:
+           # Window Theme
+            with dpg.theme_component( dpg.mvWindowAppItem ):
+                dpg.add_theme_color( dpg.mvThemeCol_WindowBg, light_bg, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_color( dpg.mvThemeCol_Border, darker_bg, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_style( dpg.mvStyleVar_WindowRounding, 10, category = dpg.mvThemeCat_Core )  # Rounded window corners
+
+            # Button Theme
+            with dpg.theme_component( dpg.mvButton ):
+                dpg.add_theme_color( dpg.mvThemeCol_Button, prim_color, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_color( dpg.mvThemeCol_ButtonHovered, ( 81, 150, 245 ), category = dpg.mvThemeCat_Core )
+                dpg.add_theme_color( dpg.mvThemeCol_ButtonActive, ( 52, 115, 220 ), category = dpg.mvThemeCat_Core )
+                dpg.add_theme_color( dpg.mvThemeCol_Text, ( 255, 255, 255 ), category = dpg.mvThemeCat_Core )  # White text on buttons
+                dpg.add_theme_style( dpg.mvStyleVar_FrameRounding, 2, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_style( dpg.mvStyleVar_FramePadding, 5, 5, category = dpg.mvThemeCat_Core )
+
+            # Float Input Theme
+            with dpg.theme_component( dpg.mvInputFloat ):
+                dpg.add_theme_color( dpg.mvThemeCol_FrameBg, darker_bg, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_color( dpg.mvThemeCol_FrameBgHovered, ( 235, 240, 255 ), category = dpg.mvThemeCat_Core )
+                dpg.add_theme_color( dpg.mvThemeCol_Text, text_color, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_style( dpg.mvStyleVar_FrameRounding, 2, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_style( dpg.mvStyleVar_FramePadding, 5, 3, category = dpg.mvThemeCat_Core )
+
+            # Text Input Theme
+            with dpg.theme_component( dpg.mvInputText ):
+                dpg.add_theme_color( dpg.mvThemeCol_FrameBg, darker_bg, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_color( dpg.mvThemeCol_FrameBgHovered, ( 235, 240, 255 ), category = dpg.mvThemeCat_Core )
+                dpg.add_theme_color( dpg.mvThemeCol_Text, text_color, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_style( dpg.mvStyleVar_FrameRounding, 2, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_style( dpg.mvStyleVar_FramePadding, 5, 3, category = dpg.mvThemeCat_Core )
+
+            # Slider Theme
+            with dpg.theme_component( dpg.mvSliderFloat ):
+                dpg.add_theme_color( dpg.mvThemeCol_SliderGrab, prim_color, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_color( dpg.mvThemeCol_FrameBg, darker_bg, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_color( dpg.mvThemeCol_FrameBgHovered, ( 235, 240, 255 ), category = dpg.mvThemeCat_Core )
+                dpg.add_theme_color( dpg.mvThemeCol_Text, text_color, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_style( dpg.mvStyleVar_FrameRounding, 2, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_style( dpg.mvStyleVar_GrabRounding, 5, category = dpg.mvThemeCat_Core )
+
+            # Radio Button Theme
+            with dpg.theme_component( dpg.mvRadioButton ):
+                dpg.add_theme_color( dpg.mvThemeCol_Button, prim_color, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_color( dpg.mvThemeCol_ButtonHovered, ( 66, 165, 245 ), category = dpg.mvThemeCat_Core )
+                dpg.add_theme_color( dpg.mvThemeCol_ButtonActive, prim_color, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_color( dpg.mvThemeCol_Text, text_color, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_style( dpg.mvStyleVar_FrameRounding, 2, category = dpg.mvThemeCat_Core )
+                dpg.add_theme_style( dpg.mvStyleVar_FramePadding, 5, 3, category = dpg.mvThemeCat_Core )
+
+        dpg.bind_theme( global_theme )
 
 tf_gui( )
